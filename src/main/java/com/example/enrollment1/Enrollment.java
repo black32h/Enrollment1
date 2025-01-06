733f656e1b896ecdeadc88c0fd1f384a42597005
black32h <blackspetya@gmail.com>
2025-01-06 21:27:28 +0200

    Update Enrollment.java

--- src/main/java/com/example/enrollment1/Enrollment.java
+++ src/main/java/com/example/enrollment1/Enrollment.java
@@ -1,4 +1,47 @@
 package com.example.enrollment1;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+
+@Entity
 public class Enrollment {
+
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId; // ID студента
+    private Long courseId; // ID курсу
+
+    // Геттер для id
+    public Long getId() {
+        return id;
+    }
+
+    // Сеттер для id
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Геттер для userId
+    public Long getUserId() {
+        return userId;
+    }
+
+    // Сеттер для userId
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    // Геттер для courseId
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    // Сеттер для courseId
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
 }
