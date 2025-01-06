d195bd0c0afe2ec197a4f8976c8a47fdb902b04d
black32h <blackspetya@gmail.com>
2025-01-06 21:29:02 +0200

    Update EnrollmentRepository.java

--- src/main/java/com/example/enrollment1/EnrollmentRepository.java
+++ src/main/java/com/example/enrollment1/EnrollmentRepository.java
@@ -1,4 +1,12 @@
 package com.example.enrollment1;
 
-public class EnrollmentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+// Інтерфейс для роботи з базою даних через JPA
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+    
+    // Метод для отримання списку записів за userId
+    List<Enrollment> findByUserId(Long userId);
 }
